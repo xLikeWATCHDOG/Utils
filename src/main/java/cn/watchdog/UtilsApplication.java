18e0c7cc69dfd694cb7d00dddcb25814c13cda8c
花花 <73741466+xLikeWATCHDOG@users.noreply.github.com>
2024-08-16 18:06:59 +0800

    feat: Add @Component

--- src/main/java/cn/watchdog/UtilsApplication.java
+++ src/main/java/cn/watchdog/UtilsApplication.java
@@ -2,7 +2,9 @@ package cn.watchdog;
 
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
+@Component
 @Slf4j
 public class UtilsApplication {
 	@PostConstruct
